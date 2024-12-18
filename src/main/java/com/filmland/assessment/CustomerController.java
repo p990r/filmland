8e00e1eebfdc0f47443c9802d4a8f7281ffd08fa
p990r <perir1@gmail.com>
2024-12-18 11:36:45 +0100

    login function

--- src/main/java/com/filmland/assessment/CustomerController.java
+++ src/main/java/com/filmland/assessment/CustomerController.java
@@ -1,8 +1,12 @@
 package com.filmland.assessment;
 
+import io.micrometer.core.ipc.http.HttpSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.filmland.assessment.Entity.Customer;
+
+import java.net.http.HttpResponse;
 
 @CrossOrigin
 @RestController
@@ -21,9 +25,15 @@ public class CustomerController {
         return this.customerService.create(customer);
     }
 
+    // Hardcoded customer for testing
     @PostMapping("test")
     public Customer create1() {
         Customer customer = new Customer("e@mail.com", "123");
         return this.customerService.create(customer);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<Object> login(@RequestBody Customer customer) {
+        return this.customerService.login(customer);
     }
 }
--- src/main/java/com/filmland/assessment/CustomerService.java
+++ src/main/java/com/filmland/assessment/CustomerService.java
@@ -1,14 +1,18 @@
 package com.filmland.assessment;
 
-//import lombok
+import com.filmland.assessment.Entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.filmland.assessment.Entity.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Transactional
-//@AllArgsC
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
@@ -19,5 +23,18 @@ public class CustomerService {
 
     public Customer create(Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    public ResponseEntity<Object> login(Customer customer) {
+        Map<String, String> body = new HashMap<>();
+        Optional<Customer> customer1 = customerRepository.findByEmail(customer.getEmail());
+        if(customer1.isPresent() && customer.getPassword().equals(customer.getPassword())) {
+            body.put("status:", "Login successful");
+            body.put("message:", "Welcome " + customer.getEmail());
+            return new ResponseEntity<>(body, HttpStatus.OK);
+        }
+        body.put("status:", "Login failed");
+        body.put("message:", "Wrong credentials");
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
