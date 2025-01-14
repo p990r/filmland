9a433685582b593bf9221da67390d5b067d1a34c
p990r <perir1@gmail.com>
2025-01-14 10:46:07 +0100

    subsctiption entity tests

--- src/test/java/com/filmland/assessment/entity/SubscriptionTest.java
+++ src/test/java/com/filmland/assessment/entity/SubscriptionTest.java
@@ -1,28 +1,34 @@
 package com.filmland.assessment.entity;
 
-import com.filmland.assessment.Entity.Customer;
 import com.filmland.assessment.Entity.Subscription;
+import com.filmland.assessment.TestModels;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SubscriptionTest {
 
     @Test
     void getCustomers(){
-
+        Subscription subscription = TestModels.getSubscription();
+        assertEquals(1, subscription.getCustomers().size());
     }
 
     @Test
     void getCategories(){
-
+        Subscription subscription = TestModels.getSubscription();
+        assertEquals(1, subscription.getCategories().size());
     }
 
     @Test
     void getStartDate(){
-
+        Subscription subscription = TestModels.getSubscription();
+        assertEquals(2001, subscription.getStartDate().getYear());
     }
 
     @Test
     void getEndDate(){
-
+        Subscription subscription = TestModels.getSubscription();
+        assertEquals(2002, subscription.getEndDate().getYear());
     }
 }
