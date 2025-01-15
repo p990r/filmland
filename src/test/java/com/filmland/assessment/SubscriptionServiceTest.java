c4b2cd105d12eec67332fb7198478b4e0d31137c
p990r <perir1@gmail.com>
2025-01-15 09:29:13 +0100

    fix

--- src/test/java/com/filmland/assessment/SubscriptionServiceTest.java
+++ src/test/java/com/filmland/assessment/SubscriptionServiceTest.java
@@ -4,13 +4,16 @@ import com.filmland.assessment.Entity.Subscription;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SubscriptionServiceTest {
     SubscriptionService subscriptionService;
 
